0de5ee777bd4600f707a5d624b50a2e569725299
commodore73 <git@c73.me>
2021-07-05 12:05:35 -0700

    Attribute to identify content type(s) for entry model class

---
+++ Deliverystack.Core/Attributes/AttributeBase.cs
@@ -0,0 +1,54 @@
+﻿namespace Deliverystack.Core.Attributes
+{
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+
+public abstract class AttributeBase : Attribute
+{
+    // map the type of the attribute to a list of types that have that attribute.
+    // static and cached to optimize performance.
+    // ReSharper disable once InconsistentNaming
+    private static readonly ConcurrentDictionary<Type, List<Type>> _types = new();
+
+    public static Type[] GetTypesWithAttribute(Type attribute)
+    {
+        // Have assemblies already been scanned for this attribute.
+        if (!_types.ContainsKey(attribute))
+        {
+            // no, create a new entry in the dictionary mapping
+            // the attribute to the list of types that have that attribute.
+            List<Type> result = new List<Type>();
+
+            // for each assembly available
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                try
+                {
+                    // for each type in the assembly
+                    foreach (Type type in assembly.GetTypes())
+                    {
+                        // if the type has the attribute
+                        if (type.GetCustomAttributes(attribute, true).Length > 0)
+                        {
+                            // then add it to the list of types for the attribute
+                            result.Add(type);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    // ignore; unable to load; assembly may be obfuscated, etc. 
+                }
+            }
+
+            // store a record mapping the attribute to the list of types with that attribute
+            _types[attribute] = result;
+        }
+
+        // return the list of types with the attribute
+        return _types[attribute].ToArray();
+    }
+}
+}
---
+++ Deliverystack.Core/Attributes/AutoLoadJsonConverter.cs
@@ -0,0 +1,38 @@
+﻿namespace Deliverystack.Core.Attributes
+{
+    using System;
+    using System.Diagnostics;
+    using System.Reflection;
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AutoLoadJsonConverter : AttributeBase
+    {
+        /// <summary>
+        /// Enabled by default.
+        /// </summary>
+        public bool Enabled { get; }
+
+        public AutoLoadJsonConverter(bool enabled = true)
+        {
+            Enabled = enabled;
+        }
+
+        public static bool IsEnabledForType(Type t)
+        {
+            Trace.Assert(t != null, "t is null");
+
+            foreach (var attr in t.GetCustomAttributes(typeof(AutoLoadJsonConverter)))
+            {
+                AutoLoadJsonConverter ctdAttr = attr as AutoLoadJsonConverter;
+                Trace.Assert(ctdAttr != null, "cast is null");
+
+                if (!ctdAttr.Enabled)
+                {
+                    return ctdAttr.Enabled;
+                }
+            }
+
+            return true;
+        }
+    }
+}
