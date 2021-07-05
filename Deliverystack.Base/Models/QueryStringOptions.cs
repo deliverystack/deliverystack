367f08040f795eccaf1f6649314682b52f196b88
commodore73 <git@c73.me>
2021-07-05 11:58:34 -0700

    it is what it is

---
+++ Deliverystack.Base/Models/QueryStringOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Deliverystack.Base.Models
+{
+    using System;
+    using System.Linq;
+
+    public abstract class QueryStringOptions
+    {
+        // cannot be a property or it will go infinite loop trying to add this property to the query string
+        public string GetQueryString()
+        {
+            return String.Join("&", GetType().GetProperties().Where(
+                prop => prop.GetValue(this, null) != null).Select(
+                    prop => $"{Uri.EscapeDataString(prop.Name)}={Uri.EscapeDataString(prop.GetValue(this).ToString())}"));
+        }
+    }
+}
